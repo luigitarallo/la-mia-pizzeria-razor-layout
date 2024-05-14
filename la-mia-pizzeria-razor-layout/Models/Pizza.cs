d2cf2542c1f52bb30b38f98865206c1b66bd2ec3
luigitarallo <tarallo.gigi@gmail.com>
2024-05-14 22:40:53 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-razor-layout/Models/Pizza.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_razor_layout.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza() { }
+
+        public Pizza(string name, string description, string photo, decimal price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Photo = photo;
+            this.Price = price;
+        }
+    }
+}
